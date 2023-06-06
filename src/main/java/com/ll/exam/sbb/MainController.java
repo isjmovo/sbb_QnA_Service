360074cdf99123e673250a50b015d73a93bfbace
isjmovo <isjmovo@gmail.com>
2023-06-06 13:11:43 +0900

    ResponseBody

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -2,11 +2,13 @@ package com.ll.exam.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
   @RequestMapping("/sbb")
-  public void index() {
-    System.out.println("index");
+  @ResponseBody
+  public String index() {
+    return "안녕하세요.";
   }
 }
