14d93238bce8d02dce4a2ce8887b6a74f54852b5
isjmovo <isjmovo@gmail.com>
2023-06-06 21:01:50 +0900

    테스트 데이터를 저장할 때마다 쌓이지 않게

--- src/main/java/com/ll/exam/sbb/AnswerRepository.java
+++ src/main/java/com/ll/exam/sbb/AnswerRepository.java
@@ -1,6 +1,13 @@
 package com.ll.exam.sbb;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+  @Transactional
+  @Modifying
+  @Query(value = "truncate answer", nativeQuery = true)
+  void truncate();
 }
--- src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
@@ -1,5 +1,6 @@
 package com.ll.exam.sbb;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,22 @@ public class AnswerRepositoryTests {
   private QuestionRepository questionRepository;
   @Autowired
   private AnswerRepository answerRepository;
+  private int lastSampleDataId;
+
+  @BeforeEach
+  void beforeEach() {
+    clearData();
+    createSampleData();
+  }
+
+  private void createSampleData() {
+  }
+
+  private void clearData() {
+    questionRepository.disableForeignKeyCheck();
+    answerRepository.truncate();
+    questionRepository.enableForeignKeyCheck();
+  }
 
   @Test
   void 저장() {
